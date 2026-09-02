37abfbac8e4a65dcd43dd44260a03cc7b9dd999b
huimin2026 <z2150211863@163.com>
2026-09-02 22:27:41 +0800

    2026年9月2日 HelloWorld入门代码

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.itheima.test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        //输出一段文本，文本内容:Hello World
+        //如果要修改输出的内容，那么直接修改双引号里面就可以
+        System.out.println("Hello World");
+    }
+}
