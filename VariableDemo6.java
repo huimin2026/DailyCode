d907df3e99ff51bc5d8f28f8af0716f5816525c9
huimin2026 <z2150211863@163.com>
2026-09-05 20:48:30 +0800

    2026/9/5

---
+++ VariableDemo6.java
@@ -0,0 +1,32 @@
+package variable;
+import java.util.Scanner;
+
+
+public class VariableDemo6 {
+    public static void main(String[] args) {
+        /*
+
+    键盘录入：
+        第一步：找到Scanner这个打工人
+        第二步：让Scanner干活
+
+     */
+
+        // 1. 找到Scanner这个打工人
+        Scanner sc = new Scanner (System.in);
+
+        // 2.让Scanner干活
+        //接收键盘录入的整数
+        /*int num = sc.nextInt();
+        System.out.println(num);*/
+
+        //接收键盘录入的小数
+        /*double num2 = sc.nextDouble();
+        System.out.println(num2);*/
+
+        //接收键盘录入的字符串
+        /*String str = sc.next();
+        System.out.println(str);*/
+
+    }
+}
