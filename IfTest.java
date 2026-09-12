6d7a319d6abd08307cc60fd21d97be73377c4942
huimin2026 <z2150211863@163.com>
2026-09-12 23:44:03 +0800

    if代码练习

---
+++ IfTest.java
@@ -0,0 +1,10 @@
+package com.itheima.ifdemo;
+
+public class IfTest {
+    public static void main(String[] args){
+        int score = 78;
+        if(score >=60){
+            System.out.println("及格");
+        }
+    }
+}
