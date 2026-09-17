330816d51b9be641cdb325e8cae215f10c89703b
huimin2026 <z2150211863@163.com>
2026-09-17 22:47:06 +0800

    for循环代码练习

---
+++ ForDemo3.java
@@ -0,0 +1,16 @@
+package loopfor;
+
+public class ForDemo3 {
+    public static void main(String[] args) {
+        //5~1
+
+        //循环的开始条件：5
+        //循环的结束条件：1
+        //重复要执行的代码：输出数字sout
+
+        for(int i=5; i>=1;i--){
+            System.out.println(i);
+        }
+
+    }
+}
