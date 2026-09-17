a93f37d2f77829c948d0c3257c24374d30504701
huimin2026 <z2150211863@163.com>
2026-09-17 21:56:37 +0800

    for循环代码练习

---
+++ ForDemo1.java
@@ -0,0 +1,21 @@
+package loopfor;
+
+public class ForDemo1 {
+    public static void main(String[] args) {
+       /*
+       实现游戏中连续跳跃10次，用输出语句模拟跳跃的逻辑思维
+       格式：
+            for（初始化语句；条件判断语句）；条件控制语句{
+                      循环体语句
+                      }
+                      三要素：
+                      初始化语句：开始条件
+                      条件判断语句：结束条件 100
+                      条件控制语句： +1 i++
+                      循坏体：重复执行的代码*/
+        for(int i = 1;i<=100;i++){
+            System.out.println("游戏人物在跳跃~");
+        }
+        }
+    }
+
