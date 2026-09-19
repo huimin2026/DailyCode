7fc2e40a6eea02f968b3ce5ffb047d9c1b9b3f99
huimin2026 <z2150211863@163.com>
2026-09-19 16:44:45 +0800

    无限循环代码

---
+++ InfiniteLoopDemo.java
@@ -0,0 +1,24 @@
+package infiniteloop;
+
+public class InfiniteLoopDemo {
+    public static void main(String[] args) {
+        /*
+        无限循环
+        注意：在无限循环的下面，不能写任何其他代码*/
+
+        //for
+        /*for(;;){
+        System.out.println("hello,world!");
+        }*/
+
+        //while
+        /*while(true){
+            System.out.println("hello,world!");
+        }*/
+
+        //do...while
+        /*do{
+            System.out.println("hello,world!");
+        }while(true);*/
+    }
+}
