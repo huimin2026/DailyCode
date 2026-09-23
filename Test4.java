6258246e06f0be2ce33815986f7259a707e90db9
huimin2026 <z2150211863@163.com>
2026-09-23 21:38:40 +0800

    平行四边形

--- Test4.java
+++ Test4.java
@@ -1,13 +1,19 @@
-package controllerloop;
+package looploop;
 
 public class Test4 {
+    public static void main(String[] args){
 
-        public static void main(String[] args) {
-            for(int i = 1; i <= 10; i++){
-                if(i == 5){
-                    break; // i等于5，直接跳出整个循环，不再继续
+                // 4行，每行5个*
+                for(int i = 1; i <= 4; i++){
+                    // 打印空格：第1行3个空格，第2行2个，第3行1个，第4行0个
+                    for(int j = 1; j <= 4-i; j++){
+                        System.out.print(" ");
+                    }
+                    // 打印5个星号
+                    for(int j = 1; j <=5; j++){
+                        System.out.print("*");
+                    }
+                    System.out.println();
                 }
-                System.out.println(i);
             }
         }
-    }
