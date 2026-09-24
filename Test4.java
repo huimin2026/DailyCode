c5ebfbe450958a30fe5161e7a695df4d04ed1697
huimin2026 <z2150211863@163.com>
2026-09-24 15:13:23 +0800

    打印平行四边形一

--- Test4.java
+++ Test4.java
@@ -1,18 +1,26 @@
 package looploop;
 
 public class Test4 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-                // 4行，每行5个*
-                for(int i = 1; i <= 4; i++){
-                    // 打印空格：第1行3个空格，第2行2个，第3行1个，第4行0个
-                    for(int j = 1; j <= 4-i; j++){
+                /*打印平行四边形
+                ******
+               ******
+              ******
+              限定：每次只能输出一个*
+              */
+
+                //外循环，3行
+                for(int i = 1; i <= 3; i++){
+                    //【第一步 打印前置空格】
+                    for(int j = 1; j <= 3 - i; j++){
                         System.out.print(" ");
                     }
-                    // 打印5个星号
-                    for(int j = 1; j <=5; j++){
+                    //【第二步 打印6个星】
+                    for(int j = 1; j <= 6; j++){
                         System.out.print("*");
                     }
+                    //【一行全部打完再换行！】
                     System.out.println();
                 }
             }
