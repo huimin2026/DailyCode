16c278f1bfcf0134ade86759f5e7ad0f0475ef13
huimin2026 <z2150211863@163.com>
2026-09-26 20:47:59 +0800

    数组练习题三 找出数组中最小值

--- Test3.java
+++ Test3.java
@@ -1,33 +1,24 @@
-package looploop;
+package array;
 
 public class Test3 {
     public static void main(String[] args) {
-          /*
-       打印正三角形
-        *
-        **
-        ***
-        ****
-        *****
-        打印倒三角形
-        *****
-        ****
-        ***
-        **
-        *
+       /* 需求：已知数组元素为{33，5，22，44，55}
+        请找出数组中最小值并打印在控制台*/
 
-        限定：每次只能输出一个*
-        */
+        //1.创建数组
+        int[] arr = {33,5,22,44,55};
 
-        //打印正三角形的第一行
+        //2.定义擂台变量min，默认拿第一个元素
+        int min = arr[0];
 
-        //外循环：把在一行打印N个星的事情，重复五次
-        for(int i = 1;i<=5;i++){
-            //内循环：在一行打印1~5个星
-            for (int j=1;j<=6-i;j++){
-                System.out.print("*");
+        //3.循环挨个比较
+        for(int i = 0;i<arr.length;i++){
+            //如果当前arr[i]比擂台min更小，就替换擂台
+            if(min >arr[i]){
+                min = arr[i];
             }
-            System.out.println();
         }
+        //4.输出结果
+        System.out.println(min);
     }
 }
