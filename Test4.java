8854c0efb1bcb8207e708b4ffe6129e344456201
huimin2026 <z2150211863@163.com>
2026-09-26 20:52:33 +0800

    前置练习 交换两个变量中的数据

--- Test4.java
+++ Test4.java
@@ -1,27 +1,21 @@
-package looploop;
+package array;
 
 public class Test4 {
     public static void main(String[] args) {
+        /*前置练习：
+              交换两个变量中的数据*/
 
-                /*打印平行四边形
-                ******
-               ******
-              ******
-              限定：每次只能输出一个*
-              */
+        int a = 10;
+        int b = 20;
 
-                //外循环，3行
-                for(int i = 1; i <= 3; i++){
-                    //【第一步 打印前置空格】
-                    for(int j = 1; j <= 3 - i; j++){
-                        System.out.print(" ");
-                    }
-                    //【第二步 打印6个星】
-                    for(int j = 1; j <= 6; j++){
-                        System.out.print("*");
-                    }
-                    //【一行全部打完再换行！】
-                    System.out.println();
-                }
-            }
-        }
+        //定义一个第三方变量
+        int temp = a;
+        //把变量b中记录的值，赋值给a
+        a = b;
+        //把变量a原来的值（temp），赋值给b
+        b = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
