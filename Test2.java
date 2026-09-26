e0310dc51368a84df31e3b5a959a76f7ff5d7822
huimin2026 <z2150211863@163.com>
2026-09-26 20:46:53 +0800

    数组练习题二 找出数组中最大值

--- Test2.java
+++ Test2.java
@@ -1,34 +1,34 @@
-package looploop;
+package array;
 
 public class Test2 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         /*
-       打印正三角形
-        *
-        **
-        ***
-        ****
-        *****
-        打印倒三角形
-        *****
-        ****
-        ***
-        **
-        *
+        需求：已知数组元素为{33，5，22，44，55}
+        请找出数组中最大值并打印在控制台
 
-        限定：每次只能输出一个*
-        */
+        细节1：
+             循环的开始条件，如果为0.请问：对结果有没有影响
+             对结果没有任何影响，只不过，循环多执行一次
 
-        //打印正三角形的第一行
+         细节2：
+              max的初始化值，能不能赋值为0？
+              max的初始化值，不能赋值为0
+              max记录的值，一定要是数组里面的元素*/
 
-        //外循环：把在一行打印N个星的事情，重复五次
-        for(int i = 1;i<=5;i++){
-            //内循环：在一行打印1~5个星
-            for (int j=1;j<=i;j++){
-                System.out.print("*");
+        //1.创建数组
+        int[] arr = {33,5,22,44,55};
+
+        //2.定义一个变量（擂台），默认记录第一个值
+        int max = arr[0];
+
+        //3.让后面的人依次跟擂台的人比较一下
+        for(int i= 1;i <arr.length;i++){
+            //判断
+            if(max < arr[i]){
+                max = arr[i];
             }
-            System.out.println();
         }
+        //4.输出
+        System.out.println(max);
     }
-
 }
